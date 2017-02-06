e706e4fcb01d2074e33afb318b0c950bfc1555b4
sleeper <survivingknowledge@gmail.com>
2017-02-06 17:18:35 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :artists
+  root 'artists#index'
+  resources :artists do
+    resources :songs, only: [:index, :show]
+  end
+
   resources :songs
 end
