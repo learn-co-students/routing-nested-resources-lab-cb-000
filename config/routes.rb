44d9ef93055d35ebc5923d7e7ffc58be4be76321
CodeAlyx <alyxanderthibodeaux@gmail.com>
2017-04-20 15:44:53 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists, only: [:index, :create, :new, :edit, :update, :destroy]
+  resources :artists, only: [:show] do
+    resources :songs, only: [:index, :show]
+  end
   resources :songs
 end
