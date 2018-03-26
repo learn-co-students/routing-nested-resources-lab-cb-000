b3ecd278cc585a6cd5202a39d9f0e0e1749a046b
petitlapin86 <skinnerpa86@yahoo.com>
2018-03-26 13:32:03 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :artists
+  resources :artists do #create a nested resource route to show all songs for an artists
+    resources :songs, only: [:index, :show] #Restrict the nested songs routes to index and show actions only.
+  end
   resources :songs
 end
