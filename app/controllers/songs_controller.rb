2e6e566cbfdf3f71d405a067c1c16389427bd8bb
Quinn Dizon <qdizon@gmail.com>
2018-09-10 15:15:23 +0000

    working

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,22 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      if !Artist.where(id: params[:artist_id]).empty?
+        @songs = Artist.find(id: params[:artist_id]).songs
+      else
+        redirect_to artists_path
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
-    @song = Song.find(params[:id])
+    @song = Song.where(id: params[:id])
+    if @song.empty?
+      @artist = Artist.find(params[:artist_id])
+      redirect_to artist_songs_path(@artist)
+    end
   end
 
   def new
@@ -50,4 +62,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
