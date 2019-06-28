877fb7eafbffe9a59b2bb199028f4a04c4f513b2
Laura Felce <crepu83@yahoo.it>
2019-06-28 11:24:24 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+
+  resources :artists do
+      # nested resource for posts
+      resources :songs, only: [:index, :show]
+    end
+
+    resources :songs
+
+    root 'songs#index'
 end
